470326a070869aa4c1f7bb0ed5f85aa7f196b814
matlas57 <matanatlas2002@gmail.com>
2025-01-29 16:08:52 -0500

    Working version of app (without storage) using map, let, find functions

--- app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
+++ app/src/main/java/com/example/shoppinglistapp/ShoppingList.kt
@@ -1,17 +1,27 @@
 package com.example.shoppinglistapp
 
+import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.material3.AlertDialog
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Delete
+import androidx.compose.material.icons.filled.Edit
 import androidx.compose.material3.BasicAlertDialog
 import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -21,6 +31,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -48,7 +59,35 @@ fun ShoppingListApp() {
         ){
             //items() defines the LazyColumn content to be a list
             items(sItems) {
-
+                item ->
+                if (item.isEditing) { //If we have an item which is being edited, open up the editing menu
+                    ShoppingItemEditor(
+                        item = item,
+                        //When editing is complete set sItems to be a copy of sItems with all of the isEditing flags to false
+                        onEditComplete = {
+                            editedName, editedQuantity -> //give var names to the lambda to be set later
+                            sItems = sItems.map{it.copy(isEditing = false)}
+                            val editedItem = sItems.find { it.id == item.id } //Find the item that's being edited
+                            //Use let to safely edit items because we have an unknown item that can be null
+                            editedItem?.let {
+                                it.name = editedName
+                                it.quantity = editedQuantity
+                            }
+                        }
+                    )
+                }
+                else {
+                    ShoppingListItem(
+                        item = item,
+                        onEditClick = {
+                            //Compare the ids of all items to this button, only set isEditing for the item with a matching id
+                            sItems = sItems.map { it.copy(isEditing = it.id == item.id)}
+                        },
+                        onDeleteClick = {
+                            sItems = sItems - item //Easily delete the current item
+                        }
+                    )
+                }
             }
         }
     }
@@ -115,3 +154,74 @@ data class ShoppingItem(
     var quantity: Int,
     var isEditing: Boolean = false
 )
+
+@Composable
+fun ShoppingItemEditor(
+    item: ShoppingItem,
+    onEditComplete: (String, Int) -> Unit //lambda callback to edit content
+) {
+    var editedName by remember { mutableStateOf(item.name) } //State to update the text field
+    var editedQuantity by remember { mutableStateOf(item.quantity.toString()) }
+    var isEditing by remember { mutableStateOf(item.isEditing) }
+
+    Row (
+        modifier = Modifier.padding(8.dp).fillMaxWidth().background(Color.Cyan).border(
+            border = BorderStroke(2.dp, Color.Cyan),
+            shape = RoundedCornerShape(20)
+        ),
+        horizontalArrangement = Arrangement.SpaceEvenly
+    ) {
+        Column {
+            BasicTextField(
+                value = editedName,
+                onValueChange = {editedName = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp) //wrapContentSize adjusts size based on content
+            )
+            BasicTextField(
+                value = editedQuantity,
+                onValueChange = {editedQuantity = it},
+                singleLine = true,
+                modifier = Modifier.wrapContentSize().padding(8.dp)
+            )
+        }
+
+        Button(
+            onClick = {
+                isEditing = false
+                onEditComplete(editedName, editedQuantity.toIntOrNull() ?: 1)
+            }
+        ) {
+            Text("Save")
+        }
+    }
+}
+
+@Composable
+fun ShoppingListItem (
+    item: ShoppingItem,
+    //Here we are using lambda expressions, concise functions that can be passed as parameters, they don't take parameters and don't return anything
+    onEditClick: () -> Unit,
+    onDeleteClick: () -> Unit
+) {
+    Row(
+        modifier = Modifier.padding(8.dp).fillMaxWidth().border(
+            border = BorderStroke(2.dp, Color.Cyan),
+            shape = RoundedCornerShape(20)
+        ),
+        horizontalArrangement = Arrangement.SpaceBetween
+    ) {
+        Text(text = item.name, modifier = Modifier.padding(8.dp))
+
+        Text(text = "Qty: ${item.quantity}", modifier = Modifier.padding(8.dp))
+
+        Row(modifier = Modifier.padding(8.dp)) {
+            IconButton(onClick = onEditClick) {
+                Icon(imageVector = Icons.Default.Edit, contentDescription = null)
+            }
+            IconButton(onClick = onDeleteClick) {
+                Icon(imageVector = Icons.Default.Delete, contentDescription = null)
+            }
+        }
+    }
+}
